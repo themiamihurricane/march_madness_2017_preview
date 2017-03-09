02c4f33bf6c2082fd999fe8071d17ce6e6babf1d
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-08 21:37:10 -0500

    Added import for React

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render () {
+        return (
+            <div className="Header">
+                <a href="http://www.themiamihurricane.com/" target="_blank"><img alt="March Madness Logo" src="" /></a>
+            </div>
+        );
+    }
+}
+
+export default Header;
