470a040a147c3e750d7f9a42b4389362deb2e5a3
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-08 22:57:36 -0500

    Toying with Card, Card Slide CSS

--- src/components/Card-Slide.js
+++ src/components/Card-Slide.js
@@ -5,7 +5,12 @@ class CardSlide extends Component {
     render () {
         return (
             <div className="CardSlide">
-
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                
             </div>
         );
     }
