1dee49ac00a956d8354dfc96b904a3ed4d4e93ff
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-12 16:28:54 -0400

    Styled Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,40 @@ class Card extends Component {
         return (
             <div className="Card">
                 <div className="cardHeader">
-                    <p>{this.props.team["Team"]}</p>
+                    <h2 className="teamName">{this.props.team["Team"]}</h2>
                 </div>
-                <div className="cardBody"></div>
+                <div className="cardBody">
+                    <div className="dataRow">
+                        <div className="dataBox">
+                            <div className="dataLabel">Points Per Game</div>
+                            <div className="dataPoint">69</div>
+                        </div>
+                        <div className="dataBox">
+                            <div className="dataLabel">Field-Goal %</div>
+                            <div className="dataPoint">45.6</div>
+                        </div>
+                    </div>
+                    <div className="dataRow">
+                        <div className="dataBox">
+                            <div className="dataLabel">Free-Throw %</div>
+                            <div className="dataPoint">72.1</div>
+                        </div>
+                        <div className="dataBox">
+                            <div className="dataLabel">Three-Point %</div>
+                            <div className="dataPoint">36.4</div>
+                        </div>
+                    </div>
+                    <div className="dataRow">
+                        <div className="dataBox">
+                            <div className="dataLabel">Turnovers per Game</div>
+                            <div className="dataPoint">13</div>
+                        </div>
+                        <div className="dataBox">
+                            <div className="dataLabel">Rebounds per Game</div>
+                            <div className="dataPoint">36</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
