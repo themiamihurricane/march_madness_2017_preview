4ea1a346d5321ec5d7f4363dbd4dc2213836a52a
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-14 08:53:05 -0400

    Starting Chart Section

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BodyText from './Body-Text.js';
 import CardSlide from './Card-Slide.js';
+import ChartSection from './Chart-Section.js';
 
 class Body extends Component {
     render () {
@@ -8,6 +9,7 @@ class Body extends Component {
             <div className="Body">
                 <BodyText />
                 <CardSlide />
+                <ChartSection />
             </div>
         );
     }
