98cddfa0b0eaf547d16df075e519456bc13aa5f4
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-15 07:45:24 -0400

    Added reducer for table

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import MenTeamReducer from './reducer-teams-men.js';
+import TeamTable from './reducer-table-team.js';
 
 const allReducers = combineReducers({
-    dataMen: MenTeamReducer
+    dataMen: MenTeamReducer,
+    tableData: TeamTable
 });
 
 export default allReducers;
---
+++ src/reducers/reducer-table-team.js
@@ -0,0 +1,6 @@
+export default function () {
+    return {
+        "team1": {},
+        "team2": {}
+    };
+}
