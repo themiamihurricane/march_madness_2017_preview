3c972904178d231b7cca5b9d0cee2dfc54e95352
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-16 17:03:33 -0400

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ class Footer extends Component {
     return (
       <div className="Footer">
         <p>Development, text and banner by <a target="_blank" href="https://twitter.com/Sherman_Hewitt">Sherman Hewitt</a></p>
+        <p>Sources: ESPN</p>
       </div>
     );
   }
