eaaa0a16f2e0e82cd13f2d0336b35fe61bbf02e4
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-17 16:17:58 -0400

    Updated text

--- src/components/Body-Text.js
+++ src/components/Body-Text.js
@@ -4,8 +4,9 @@ class BodyText extends Component {
     render () {
         return (
             <div className="BodyText">
-                <p>Making its second March Madness run in a row, the Miami Hurricanes (21-11) look to face the Michigan State Spartans (19-14) tonight.</p>
+                <p>Making its second March Madness run in a row, the Miami Hurricanes (21-11) look to face the Michigan State Spartans (19-14) tonight. Key wins against North Carolina, Virginia and Duke prior to the ACC Tournament show that the Canes have the potential to make a decent run in the NCAA Tournament. </p>
                 <p>Up to this point, top scorers for the Canes have been senior guard Davon Reed, junior guard Ja’Quan Newton and freshman guard Bruce Brown. For more stats about the team, check out the slider below.</p>
+                <p>“I still don’t even think we have played our best basketball yet, that’s the best part about it,” Reed said in an <a target="_blank" href="http://www.themiamihurricane.com/2017/03/12/hurricanes-are-no-8-seed-going-into-ncaa-tournament/">interview with the Hurricane</a>. “We can start to get the ball rolling right now.”</p>
             </div>
         );
     }
