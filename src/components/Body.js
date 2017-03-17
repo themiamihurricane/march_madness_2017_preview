2eeefb9a3b22c448228f87c547a02657153138e5
SHewitt95 <s.hewitt7@umiami.edu>
2017-03-17 14:01:12 -0400

    Changed card colors

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,7 +11,6 @@ class Body extends Component {
                 <BodyText />
                 <PlayerCardSlide />
                 <ChartSection />
-                <CardSlide />
             </div>
         );
     }
